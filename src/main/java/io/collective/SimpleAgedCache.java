aa18dfe92ef9d96477677aa71a668e50a6ef5198
lewis sneddon <lewis@pop-os.localdomain>
2023-05-12 19:22:04 +0100

    defined node

--- src/main/java/io/collective/SimpleAgedCache.java
+++ src/main/java/io/collective/SimpleAgedCache.java
@@ -4,7 +4,10 @@ import java.time.Clock;
 
 public class SimpleAgedCache {
 
+    private Option<ExpirableEntry> head;
+
     public SimpleAgedCache(Clock clock) {
+        this.head = Option.empty()
     }
 
     public SimpleAgedCache() {
@@ -23,5 +26,38 @@ public class SimpleAgedCache {
 
     public Object get(Object key) {
         return null;
+    }
+
+    class ExpirableEntry {
+        private Object key;
+        private Object value;
+        private Clock clock;
+        private Option<ExpirableEntry> next;
+
+        public ExpirableEntry(Object key, Object value, Clock clock) {
+            this.key = key;
+            this.value = value;
+            this.clock = clock;
+        }
+
+        public Option<ExpirableEntry> getNext() {
+            return next;
+        }
+
+        public void setNext(Option<ExpirableEntry> next) {
+            this.next = next;
+        }
+
+        public Object getKey() {
+            return key;
+        }
+
+        public Object getValue() {
+            return value;
+        }
+
+        public Clock getClock() {
+            return clock;
+        }
     }
 }
